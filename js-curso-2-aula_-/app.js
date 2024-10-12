d3ac6bf922c98959cdcf0588797a108bcfba3cd9
Felipe Martins <felipe.mart.pereira@gmail.com>
2024-10-12 20:47:56 -0300

    aumentando o valor máximo de escolha para 100

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -51,7 +51,7 @@ function limparCampo() {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 function reiniciarJogo(){
